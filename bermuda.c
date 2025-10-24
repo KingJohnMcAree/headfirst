c50463281b0f6241f028eaf2961b105f9940c187
johnmcaree <31718353+johnmcaree@users.noreply.github.com>
2025-10-24 16:58:37 +0000

    adding the bermuda programme

---
+++ bermuda.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+float latitude;
+float longitude;
+char info[80];
+
+int started =0;
+
+puts("data=[");
+while (scanf("%f,%f,%79[^\n]",&latitude,&longitude,info)==3)
+    if ((latitude > 26) && (latitude < 34))
+        if ((longitude > -76)&&(longitude <
+             -64))
+            printf("latitude: %f, longitude %f, speed %s\n",latitude,longitude,info);
+return 0;
+}
+
+
