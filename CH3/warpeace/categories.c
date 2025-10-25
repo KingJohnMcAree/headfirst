84225bebffc4a5332491e043db2f32dd03247921
johnmcaree <31718353+johnmcaree@users.noreply.github.com>
2025-10-25 10:33:50 +0000

    war and peace line counts

--- CH3/warpeace/categories.c
+++ CH3/warpeace/categories.c
@@ -13,14 +13,27 @@ int main (int argc, char *argv[]){
     FILE *file1 = fopen(argv[2],"w");
     FILE *file2 = fopen(argv[4],"w");
     FILE *file3 = fopen(argv[5],"w");
+    
+    int count1=0;
+    int count2=0;
+    int count3=0;
 
     while (fscanf(in,"%79[^\n]\n",line)==1){
-        if (strstr(line,argv[1]))
+        if (strstr(line,argv[1])){
             fprintf(file1, "%s\n", line);
-        else if (strstr(line,argv[3]))
+            count1 +=1;
+        }
+        else if (strstr(line,argv[3])){
             fprintf(file2, "%s\n", line);
-        else
+            count2 +=1;
+        }
+        else{
             fprintf(file3, "%s\n", line);
-}   
+            count3 +=1;
+        }
+}
+int total = count1 + count2 + count3;
+printf("%s: %d\n%s: %d\nOthers: %d\n",argv[1], count1, argv[3], count2, count3);
+printf("Total lines:%d\n\n",total);
 return 0;
 }
