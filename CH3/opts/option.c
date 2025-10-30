b333cbba7a40a8380f21422d6507ed230215642b
johnmcaree <31718353+johnmcaree@users.noreply.github.com>
2025-10-30 00:31:55 +0000

    debugging with launch.json and tasks.json

--- CH3/opts/option.c
+++ CH3/opts/option.c
@@ -1,10 +1,19 @@
+// ...existing code...
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
 /*
 Note the getopt.h file is only needed to clear the erros in the editor
-The programme still runs without it. See ChatGPT for an explanation on POSIX C)
+The programme still runs without it. (See ChatGPT for an explanation on POSIX C)
+run the debugger: gdb --args ./options -t -d home cheese olives
+
 */
+
+static void usage(const char *prog) {
+    fprintf(stderr, "Usage: %s [-t] [-d delivery] [ingredients...]\n", prog);
+}
+
+// ...existing code...
 int main(int argc, char *argv[]) {
     char *delivery = "";
     int thick = 0;
@@ -12,7 +21,7 @@ int main(int argc, char *argv[]) {
     int ch;  // getopt returns int
 
     // Parse options: -d <arg> and -t
-    while ((ch = getopt(argc, argv, "d:t")) != EOF) {
+    while ((ch = getopt(argc, argv, ":d:t")) != -1) {
         switch (ch) {
             case 'd':
                 delivery = optarg;
@@ -20,9 +29,14 @@ int main(int argc, char *argv[]) {
             case 't':
                 thick = 1;
                 break;
+            case ':': // missing option argument
+                fprintf(stderr, "option -%c requires an argument\n", optopt);
+                usage(argv[0]);
+                return 1;
             case '?':  // unknown option
             default:
-                fprintf(stderr, "unknown option: '-%c'\n", ch == '?' ? optopt : ch);
+                fprintf(stderr, "unknown option: '-%c'\n", optopt);
+                usage(argv[0]);
                 return 1;
         }
     }
